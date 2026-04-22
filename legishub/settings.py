a69f4fdcf07ee2ab7dc8581cf2cdf20afa7f6620
beedrimon <beedrimon19@gmail.com>
2026-04-22 23:06:23 +0800

    TRANSFER SQLITE3 TO POSTGRESQL

--- legishub/settings.py
+++ legishub/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'legishub.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'legishub_db',
+        'USER': 'legishub_admin',
+        'PASSWORD': 'your_secure_password', 
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
