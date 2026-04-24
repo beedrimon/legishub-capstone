795d7c954fc112f0df7d317fd51945fb47e42c7a
Jake Hasley Hafalla <jake.hasley26@gmail.com>
2026-04-25 06:53:00 +0800

    added base html for panels except archive and settings

--- legishub/settings.py
+++ legishub/settings.py
@@ -80,9 +80,9 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
         'PORT': os.getenv('DB_PORT'),
-        #'OPTIONS': {
-        #    'sslmode': 'require',
-        #},
+        'OPTIONS': {
+           'sslmode': 'require',
+        },
     }
 }
 
